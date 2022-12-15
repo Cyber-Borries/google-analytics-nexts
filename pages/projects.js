3b0e838ac04c859725012ec0d3fbb6dc026bfccc
Cyber-Borries <69439807+Cyber-Borries@users.noreply.github.com>
2022-12-15 12:23:50 +0200

    Add files via upload

--- pages/projects.js
+++ pages/projects.js
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import Navbar from "../components/navbar";
 
-export default () => (
+export default function projects() {
   <div>
     <style global jsx>
       {`
@@ -104,5 +104,5 @@ export default () => (
         </span>
       </div>
     </div>
-  </div>
-);
+  </div>;
+}
