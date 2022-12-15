6a3c641351fd7ea8772db82fd9288cffc1f48b13
Cyber-Borries <adriaan.bornman1@gmail.com>
2022-12-15 12:11:13 +0200

    commit

---
+++ components/tracking.js
@@ -0,0 +1,7 @@
+//created a navbar component to be used on all pages
+
+import Script from "next/script";
+
+export default function Tracking(props) {
+  return <></>;
+}
---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+import Script from "next/script";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      {" "}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-40Y0MFMRQ7`}
+      />
+      ;
+      <Script strategy="lazyOnload">
+        {`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-40Y0MFMRQ7');`}
+      </Script>
+      ; <Component {...pageProps} />;
+    </>
+  );
+}
