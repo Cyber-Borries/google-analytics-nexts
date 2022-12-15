e20adef4a7a7131304c8ebedfea47939f86bf75f
Cyber-Borries <69439807+Cyber-Borries@users.noreply.github.com>
2022-12-15 12:31:00 +0200

    Add files via upload

---
+++ _app.tsx
@@ -0,0 +1,27 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+import Script from "next/script";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      {" "}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-40Y0MFMRQ7`}
+        id="google-tag"
+      />{" "}
+      <Script strategy="lazyOnload" id="google-tag-2">
+        {`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-40Y0MFMRQ7');`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+App.displayName = "myApp";
